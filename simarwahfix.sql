c0ed5f914cde8474c68c46ef3ffd24aef10c96d9
Nurul Rachmayani Sadi <rachmayaninurul24@gmail.com>
2020-10-08 17:30:05 +0700

    new database

--- simarwahfix.sql
+++ simarwahfix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Sep 2020 pada 11.25
+-- Waktu pembuatan: 08 Okt 2020 pada 12.25
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.3
 
@@ -44,10 +44,17 @@ CREATE TABLE `anggota_himpunan` (
 --
 
 INSERT INTO `anggota_himpunan` (`nim`, `nama`, `alamat`, `jenis_kelamin`, `kontak`, `email`, `jabatan`, `parent_himpunan`, `parent_bidang`) VALUES
-('1155090000', 'Muhammad Luthfi Aziz', '', 'L', '', '', 'KETUA', 'HIMATIF', 'PAOHIMATIF'),
-('1177050100', 'pucuk', 'jkrt', 'P', '0898334873', 'jeru1k@gmail.com', 'KETUA BIDANG', 'PGSD', 'Sosroh'),
-('1177050101', 'jeruk', 'bandung', 'P', '089813', 'asd@gmail.com', 'ANGGOTA', 'PGSD', 'Sosroh'),
-('1177050102', 'jeruk', 'jakarta', 'L', '0898765', 'jeruk@gmail.com', 'ANGGOTA', 'PGSD', 'Sosroh');
+('1127050100', 'dimas', '', 'L', '08982736531', 'dimasta@gmail.com', 'KETUA BIDANG', 'Kimia', 'PAO'),
+('1157050083', 'Nm', '', 'L', '', '', 'KETUA', 'HIMANETRO', 'Sosroh'),
+('1157050093', 'qw', '', 'P', '', '', 'SEKRETARIS', 'HIMANETRO', 'Sosroh'),
+('11570501012', 'qw', '', 'L', '', '', 'KETUA', 'HIMANETRO', 'ADFOKOM'),
+('1177050093', 'as', '', 'L', '', '', 'BENDAHARA', 'HIMATIF', 'EXOFFHIMATIF'),
+('1177050100', 'jeruk', '', 'L', '', '', 'KETUA', 'HIMATIF', 'ADFOKOMHIMATIF'),
+('1177050101', 'DF', '', 'P', '', '', 'KETUA', 'HIMATIF', 'EXOFFHIMATIF'),
+('1177050102', 'asd', '', 'P', '', '', 'SEKRETARIS', 'HIMATIF', 'ADFOKOMHIMATIF'),
+('11770501023', 'asd', 'bandung', 'L', '08982736531', 'jeruk@gmail.com', 'ANGGOTA', 'HIMATIF', 'ADFOKOMHIMATIF'),
+('1197050100', 'QS', '', 'P', '', '', 'ANGGOTA', 'PGSD', 'ADFOKOM PGSD'),
+('1197050101', 'HM', '', 'L', '', '', 'KETUA BIDANG', 'PGSD', 'ADFOKOM PGSD');
 
 -- --------------------------------------------------------
 
@@ -69,10 +76,11 @@ CREATE TABLE `fakultas` (
 --
 
 INSERT INTO `fakultas` (`kode_fakultas`, `nama_fakultas`, `deskripsi`, `visi`, `misi`, `parent_univ`) VALUES
-('contohfakultas', 'contoh fakultas', 'di uin', 'jir', 'jir', NULL),
-('SAINTEK', 'Sains dan Teknologi', 'Deskripsi Saintek', 'Visi Saintek', 'Misi Saintek', NULL),
-('SYARKUM', 'Syariah dan Hukum', '', '', '', NULL),
-('Tarbiyah', 'Tarbiyah dan Keguruan', 'Mulai ada sejak awal IAIN dibentuk', 'visinya ini', 'misinya ini', NULL);
+('SAINTEK', 'Sains dan Teknologi', 'Since 2010', 'sainteksatu', 'sainteksatu', NULL),
+('Tarbiyah', 'Tarbiyah dan Keguruan', '', '', '', NULL),
+('UKK', 'Unit Kegiatan Khusus', '', '', '', NULL),
+('UKM', 'Unit Kegiatan Mahasiswa', 'bla bla bla', '', '', NULL),
+('Ushuluddin', 'Ushuluddin', '', '', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -96,13 +104,19 @@ CREATE TABLE `jurusan` (
 --
 
 INSERT INTO `jurusan` (`kode_himpunan`, `nama_himpunan`, `desc_himpunan`, `visi`, `misi`, `jml_mhsaktif`, `image`, `parent_fakultas`) VALUES
-('Contoh', 'HIMANETRO', 'OK', 'visi fakultas', 'misi fakultas', '0', 'ff9c68a152720d52989b985eaed44616.jpeg', 'SAINTEK'),
-('DEMAFSAINTEK', 'DEMAF', 'Dewan Mahasiswa Fakultas Saintek', '', '', '0', '0435035e9a852837df6ae8f2097cf33d.png', 'SAINTEK'),
-('DEMAU', 'DEMAU', 'Dewan Mahasiswa Universitas', '', '', '0', 'a213b91829a3ee60d95e2b53f76003e2.jpg', NULL),
-('HIMASAKI', 'HIMASAKI', 'Himpunan Mahasiswa Kimia', 'Pinter Ngoding Pisan', '', '0', 'eddd22847be566f592b58be2c2350c7e.png', 'SAINTEK'),
-('HIMATIF', 'HIMATIF', 'Himpunan Mahasiswa Teknik Informatika', 'Visi Himatif Update ', 'Misi Himatif Update ', '0', '9b5d664b92465eb5265038ba65331334.PNG', 'SAINTEK'),
-('ILHUM', 'ILHUM', 'Ilmu Hukum', '', '', '0', '49192782891da543567e070a77e9ca62.jpg', 'SYARKUM'),
-('PGSD', 'PSGD', '', 'Visi Himatif Update k', 'Misi Himatif Update ', '', 'db5f08c6d02598be878609e792325cce.jpg', 'Tarbiyah'),
+('DEMAU', 'DEMAU', 'Dema Mahasiswa Universitas', '', '', '', '005ef1cbc869f611acf8cf13d160f959.jpg', NULL),
+('HIMANETRO', 'Himpunan Mahasiswa Teknik Elektro', '', 'visi elektro', 'misi elektro', '', '114957ea22a8959998b0dcdab5608212.jpg', 'SAINTEK'),
+('HIMASAKI', 'Himpunan Mahasiswa Fisika', '', '', '', '', '31ebb51f1c3f4feed5ab80087f636ff0.jpg', 'SAINTEK'),
+('HIMATIF', 'Himpunan Mahasiswa Teknik Informatika', '', 'visi himatif', 'misi himatif', '', '867cc687efb6f0d4944ac50b9a46d329.jpg', 'SAINTEK'),
+('Ilmu Hadist', 'Ilmu Hadist', '', '', '', '', '866340740b81f9c837ebff2a70ac539f.jpg', 'Ushuluddin'),
+('Kimia', 'kimia', '', 'visi', 'misi', '', 'c36e150a184398f0f355918ee21ca678.jpg', 'SAINTEK'),
+('KM-HB', 'Keluarga Mahasiswa Himpunan Biologi', '', '', '', '', '0eba58501f44fa87a7e4d9ba6912aaf6.jpg', 'SAINTEK'),
+('Matematika', 'Matematika Murni', '', '', '', '', '7899c22d4ea5c6f98ede3db50ea3994f.png', 'SAINTEK'),
+('PAI', 'pai', '', '', '', '', 'fe325e1ef0ddfb234806fff35df9b88b.jpg', 'Tarbiyah'),
+('PenKimia', 'Pendidikan Kimia', '', '', '', '', 'f1d7e759c6e41bb21e85a147672ca045.png', 'Tarbiyah'),
+('Perbandingan Agama', 'Perbandingan Agama', '', '', '', '', '76d4ba9b66e537273e63e67071498379.jpg', 'Ushuluddin'),
+('PGSD', 'Perguruan SD', '', '', '', '', 'aace5ce95d05333b6de821b26612b15c.png', 'Tarbiyah'),
+('PRAMUKA', 'PRAMUKA', '', '', '', '', '99c57da6d47e9ddb1bf8e65a667588c1.jpg', 'UKM'),
 ('SEMAU', 'SEMAU', 'Senat Mahasiswa Universitas', 'xxxx', 'Test', '0', '5580f7bbef60dc1f86cd0e3ab7a9e372.PNG', NULL);
 
 -- --------------------------------------------------------
@@ -124,10 +138,12 @@ CREATE TABLE `nama_bidang` (
 --
 
 INSERT INTO `nama_bidang` (`kode_bidang`, `label_bidang`, `desc_bidang`, `parent_himpunan`, `image`) VALUES
-('BIROHIMATIF', 'Biro', 'Birokrasi', 'HIMATIF', 'b963e16d2434783741ae269e235b8b9f.png'),
-('PAOHIMATIF', 'PAO', 'Pengembangan Aparatur Organisasi', 'HIMATIF', 'bac47357f2c3ce876d4490323dada2c7.jpeg'),
-('PAOILHUM', 'PAO', '', 'ILHUM', ''),
-('Sosroh', 'Sosroh', 'Sosial dan Rohani', 'PGSD', '9fb078b4a011b9fd2c99a6791b99ead8.jpg');
+('ADFOKOM', 'ADFOKOM', 'Advokasi dan Komunikasi', 'HIMANETRO', '8b6a85c4732ccd10161a5cb1ef83870d.jpg'),
+('ADFOKOM PGSD', 'ADFOKOM', 'Advokasi dan Komunikasi', 'PGSD', '2854d53a4d7697a6f2aefae0dc592e5e.jpg'),
+('ADFOKOMHIMATIF', 'ADFOKOMHIMATIF', 'ADFOKOM', 'HIMATIF', 'd785c3e7a68f59ac6cdab7595fd946a1.jpg'),
+('EXOFFHIMATIF', 'EXOFFHIMATIF', 'EX OFFICIO HIMATIF', 'HIMATIF', '4efd79ba58ff28861f118154ea8b13a8.jpg'),
+('PAO', 'PAO', 'peaaoo', 'Kimia', '7b5935cca26aceca89589b7cdfd71da8.jpg'),
+('Sosroh', 'Sosroh', 'Sosial dan Rohani', 'HIMANETRO', 'e5c3f86dab21bfd1113ca9203abe8f84.jpg');
 
 -- --------------------------------------------------------
 
@@ -185,14 +201,20 @@ CREATE TABLE `tb_detailuser` (
 --
 
 INSERT INTO `tb_detailuser` (`id_dana`, `kd_jrsn`, `kd_fklts`, `jurusan`, `statususer`, `pesangagal`, `nPengajuan`, `danaawal`, `danasisa`, `tahunakademik`, `insertdata`) VALUES
-(14, 'ILHUM', 'SYARKUM', NULL, 1, 'Alasan tidak ACC', 1, 13000000, 13000000, 2021, '2020-09-23 18:06:28'),
-(15, 'HIMATIF', 'SAINTEK', NULL, 1, 'Alasan tidak ACC', 1, 20000000, 20000000, 2021, '2020-09-22 19:53:19'),
-(16, 'HIMASAKI', 'SAINTEK', NULL, 1, 'Alasan tidak ACC', 1, 12000000, 12000000, 2021, '2020-09-22 19:53:14'),
-(17, 'DEMAFSAINTEK', 'SAINTEK', NULL, 1, 'Alasan tidak ACC', 1, 19000000, 19000000, 2020, '2020-09-22 19:53:08'),
-(18, 'Contoh', 'SAINTEK', NULL, 1, 'Alasan tidak ACC', 1, 19000000, 19000000, 2020, '2020-09-21 17:59:05'),
-(19, 'DEMAU', NULL, NULL, 1, 'Alasan tidak ACC', 1, 18000000, 18000000, 2020, '2020-09-23 11:57:50'),
-(20, 'SEMAU', NULL, NULL, 1, 'Alasan tidak ACC', 1, 19000000, 19000000, 2020, '2020-09-23 18:06:21'),
-(21, 'PGSD', 'Tarbiyah', NULL, 2, 'yang bener ya lain kalee', 2, 13000000, 9000000, 2021, '2020-09-25 09:01:18');
+(20, 'SEMAU', NULL, NULL, 1, 'Alasan tidak ACC', 1, 14000000, 14000000, 2020, '2020-09-27 17:09:33'),
+(23, 'DEMAU', NULL, NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-29 11:47:03'),
+(24, 'HIMATIF', 'SAINTEK', NULL, 2, '\'Alasan tidak ACC\'', 1, 10000000, 8000000, 2021, '2020-10-08 09:02:04'),
+(26, 'HIMANETRO', 'SAINTEK', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-29 14:21:32'),
+(27, 'PGSD', 'Tarbiyah', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 13:01:48'),
+(28, 'HIMASAKI', 'SAINTEK', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:12:33'),
+(29, 'Kimia', 'SAINTEK', NULL, 5, '\'Alasan tidak ACC\'', 1, 19000000, 11000000, 2021, '2020-10-08 10:04:50'),
+(30, 'KM-HB', 'SAINTEK', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:13:56'),
+(31, 'PenKimia', 'Tarbiyah', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:14:32'),
+(32, 'Matematika', 'SAINTEK', NULL, 1, '\'Alasan tidak ACC\'', 1, 19000000, 19000000, 2021, '2020-10-06 08:49:15'),
+(33, 'Perbandingan Agama', 'Ushuluddin', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:16:29'),
+(34, 'Ilmu Hadist', 'Ushuluddin', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:17:12'),
+(35, 'PAI', 'Tarbiyah', NULL, 1, '\'Alasan tidak ACC\'', 0, 0, 0, 0000, '2020-09-30 14:17:50'),
+(36, 'PRAMUKA', 'UKM', NULL, 1, '\'Alasan tidak ACC\'', 1, 17000000, 17000000, 2021, '2020-10-02 14:14:21');
 
 -- --------------------------------------------------------
 
@@ -246,29 +268,6 @@ INSERT INTO `tb_menuuser` (`id`, `status_id`, `menu_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_namafakultas`
---
-
-CREATE TABLE `tb_namafakultas` (
-  `id_namafakultas` int(11) NOT NULL,
-  `kode_namafakultas` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `nama_fakultas` varchar(255) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `tb_namafakultas`
---
-
-INSERT INTO `tb_namafakultas` (`id_namafakultas`, `kode_namafakultas`, `nama_fakultas`) VALUES
-(1, 'contohfakultas', 'contoh fakultas'),
-(2, 'SAINTEK', 'Sains dan Teknologi'),
-(3, 'SYARKUM', 'Syariah dan Hukum'),
-(4, 'Tarbiyah', 'Tarbiyah dan Keguruan'),
-(5, 'Universitas', 'UNIV');
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `tb_pengajuan`
 --
 
@@ -296,6 +295,13 @@ CREATE TABLE `tb_pengajuan` (
   `insertdata` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `tb_pengajuan`
+--
+
+INSERT INTO `tb_pengajuan` (`id_pengajuan`, `kd_jrsn`, `kd_fakultas`, `jurusan`, `tahunakademik`, `nPengajuan`, `statususer`, `akhirkegiatan`, `tglmakslaporan`, `tgluploadlpj`, `namaKegiatan`, `danaawal`, `danasisa`, `danaacc`, `suratpengajuan`, `rinciankegiatan`, `rkakl`, `tor`, `laporankegiatan`, `laporanrincianbiaya`, `insertdata`) VALUES
+(24, 'Kimia', 'SAINTEK', '', 2021, 1, 5, '2020-12-10', '2020-12-17', '2020-10-08', 'PBAK, AUDIENSI, ', 19000000, 11000000, 8000000, 'SPJ-201008-bee87c62c0.pdf', 'RKG-201008-bee87c62c0.pdf', 'RKA_KL-201008-bee87c62c0.pdf', 'TOR-201008-bee87c62c0.pdf', 'LPJ-201008-f2c39a8edd.pdf', 'RBY-201008-f2c39a8edd.pdf', '2020-10-08 10:04:50');
+
 -- --------------------------------------------------------
 
 --
@@ -317,6 +323,27 @@ INSERT INTO `tb_status` (`id_status`, `Nama Status`) VALUES
 (3, 'Proses Pengajuan Dana'),
 (4, 'Proses Laporan Kegiatan'),
 (5, 'Verifikasi Laporan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_sumtotalanggotafak`
+--
+
+CREATE TABLE `tb_sumtotalanggotafak` (
+  `id` int(11) NOT NULL,
+  `kd_fakultas` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `nama_fakultas` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `total_anggota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_sumtotalanggotafak`
+--
+
+INSERT INTO `tb_sumtotalanggotafak` (`id`, `kd_fakultas`, `nama_fakultas`, `total_anggota`) VALUES
+(0, 'Tarbiyah', 'Tarbiyah dan Keguruan', 0),
+(0, 'Ushuluddin', 'Ushuluddin', 0);
 
 -- --------------------------------------------------------
 
@@ -343,12 +370,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `email`, `username`, `password`, `role`, `kode_himp`, `is_active`, `statususer`, `update_date`, `insert_date`) VALUES
-(10, 'admin', 'admin@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 0, 'HIMATIF', 1, 1, '2020-09-05 00:00:00', '2020-09-05 00:00:00'),
-(12, 'Nisvy Syabana Nugraha', 'nisvy@gmail.com', 'nisvysyan', '1658175c5dfbe416e5dbbc7863ddcc90', 0, 'ILHUM', 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(13, 'nisvy', 'nisvysyabana@gmail.com', 'nisvy', 'cabaa1823f3f04eed79c753a6f5f4845', 0, 'HIMATIF', 1, 1, '0000-00-00 00:00:00', '2020-09-08 10:23:10'),
 (17, 'admin aljamiah', 'admin@gmail.com', 'admin1', 'e00cf25ad42683b3df678c61f42c6bda', 1, 'Contoh', 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(22, 'admin2', 'email@gmail.com', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 1, '', 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(29, 'PGSD', 'jeruk@gmail.com', 'PGSD', '115b3860d663b4206ee6dfd929e72a93', 0, 'PGSD', 1, 2, '0000-00-00 00:00:00', '2020-09-19 19:26:18');
+(22, 'admin2', 'email@gmail.com', 'admin2', 'c84258e9c39059a89ab77d846ddab909', 1, '', 1, 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(30, 'if', 'jeruk@gmail.com', 'ifsakti', '929d97cf81785edc0ff40c0fcad87726', 0, 'HIMATIF', 1, 2, '0000-00-00 00:00:00', '2020-09-29 16:22:35'),
+(31, 'elektro', 'asd@gmail.com', 'elektro', '3f0062caa096feac56117e460aa91bdd', 0, 'HIMANETRO', 1, 2, '0000-00-00 00:00:00', '2020-09-29 16:23:03'),
+(32, 'PGSD', 'jeruk@gmail.com', 'pgsd', '115b3860d663b4206ee6dfd929e72a93', 0, 'PGSD', 1, 2, '0000-00-00 00:00:00', '2020-09-30 15:02:19'),
+(33, 'kimiasains', 'kimia@uinsgd.ac.id', 'kimiasains', '5c913be9b7d3732443786aeebbad0818', 0, 'Kimia', 1, 5, '0000-00-00 00:00:00', '2020-10-08 11:49:26');
 
 --
 -- Indexes for dumped tables
@@ -409,13 +436,6 @@ ALTER TABLE `tb_menuuser`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_namafakultas`
---
-ALTER TABLE `tb_namafakultas`
-  ADD PRIMARY KEY (`id_namafakultas`),
-  ADD KEY `fak` (`kode_namafakultas`);
-
---
 -- Indeks untuk tabel `tb_pengajuan`
 --
 ALTER TABLE `tb_pengajuan`
@@ -428,6 +448,12 @@ ALTER TABLE `tb_pengajuan`
 --
 ALTER TABLE `tb_status`
   ADD PRIMARY KEY (`id_status`);
+
+--
+-- Indeks untuk tabel `tb_sumtotalanggotafak`
+--
+ALTER TABLE `tb_sumtotalanggotafak`
+  ADD KEY `kd_fakultas` (`kd_fakultas`);
 
 --
 -- Indeks untuk tabel `user`
@@ -445,7 +471,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `tb_detailuser`
 --
 ALTER TABLE `tb_detailuser`
-  MODIFY `id_dana` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_dana` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_keluhan`
@@ -454,22 +480,16 @@ ALTER TABLE `tb_keluhan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_namafakultas`
---
-ALTER TABLE `tb_namafakultas`
-  MODIFY `id_namafakultas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT untuk tabel `tb_pengajuan`
 --
 ALTER TABLE `tb_pengajuan`
-  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
